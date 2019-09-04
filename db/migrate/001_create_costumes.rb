57b9d9144035cfbd627961c80076df000862bbc5
Christian Polanco <chrispolanco@sbcglobal.net>
2019-09-04 05:50:25 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,3 +1,14 @@
+class CreateCostumes < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costumes do |t|
+      t.float :price
+      t.string :name
+      t.string :image_url
+      t.string :size
 
+      t.timestamps null: false
+    end
+  end
+end
 
 
